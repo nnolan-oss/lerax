b30c398d7cb28fbfee6f4b61fcafacc356e5a62a
Javoxir G'ulomjonov <javohirtech@gmail.com>
2025-09-29 21:06:19 +0500

    feat: golang index page

--- app/components/app-navbar/AppNavbar.vue
+++ app/components/app-navbar/AppNavbar.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="border-b border-b-primary-500/10 sticky top-0 backdrop-blur-lg bg-opacity-50">
+  <div
+    class="border-b border-b-primary-500/10 sticky top-0 backdrop-blur-lg bg-opacity-50 z-50"
+  >
     <div class="my-container flex justify-between items-center">
       <NuxtLink href="/" class="font-bold font-mono">
         <span class="text-primary">{</span>
@@ -8,11 +10,15 @@
       </NuxtLink>
       <div class="space-x-2 flex items-center">
         <UButton href="/examples">Examples</UButton>
-        <ToggleMode/>
-        <UButton icon="tabler:brand-github" size="md" color="primary" variant="soft"/>
+        <ToggleMode />
+        <UButton
+          icon="tabler:brand-github"
+          size="md"
+          color="primary"
+          variant="soft"
+        />
       </div>
     </div>
   </div>
 </template>
-<script setup lang="ts">
-</script>
+<script setup lang="ts"></script>
