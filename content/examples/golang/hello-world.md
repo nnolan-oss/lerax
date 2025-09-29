b83d7f7e3508b5f012e3dd6cfb9547ef0bca9dc2
Javoxir G'ulomjonov <javohirtech@gmail.com>
2025-09-29 21:34:26 +0500

    chore: code hightlight

---
+++ content/examples/golang/hello-world.md
@@ -0,0 +1,21 @@
+---
+title: Hello World
+summary: Dasturlash tilini o'zbek tilida namunalar bilan o'rganish
+date: 2025-09-29
+---
+
+<div class="my-md-content">
+## Hello World
+
+Keling an'anamizda sodiq qolib, "hello world" dasturini yozib ko'ramiz
+
+```go
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("hello world")
+}
+```
+</div>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,9 +3,39 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
-  devtools: {enabled: true},
+  devtools: { enabled: true },
   css: ['~/assets/css/main.css'],
-  vite: {plugins: [tailwindcss(),],},
+  vite: { plugins: [tailwindcss()], },
+
+  content: {
+    build: {
+      markdown: {
+        toc: {
+          depth: 3,
+        },
+        highlight: {
+          langs: [
+            "go",
+            "php",
+            "js",
+            "jsx",
+            "ts",
+            "tsx",
+            "python",
+            "dart",
+          ],
+          theme: {
+            // Default theme (same as single string)
+            default: 'github-light',
+            // Theme used if `html.dark`
+            dark: 'github-dark',
+            // Theme used if `html.sepia`
+            sepia: 'monokai'
+          }
+        }
+      }
+    }
+  },
 
   modules: [
     '@nuxt/content',
