c55dc8ad427dfcafacdc746d9e093897ef0d7fd8
Javoxir G'ulomjonov <javohirtech@gmail.com>
2025-09-29 10:56:06 +0500

    feat: added new docs t ohome

--- app/components/app-navbar/AppNavbar.vue
+++ app/components/app-navbar/AppNavbar.vue
@@ -1,10 +1,14 @@
 <template>
   <div class="border-b border-b-primary-500/10 sticky top-0 backdrop-blur-lg bg-opacity-50">
     <div class="my-container flex justify-between items-center">
-      <NuxtLink href="/" class="font-bold font-mono">lerax</NuxtLink>
+      <NuxtLink href="/" class="font-bold font-mono">
+        <span class="text-primary">{</span>
+        lerax
+        <span class="text-primary">}</span>
+      </NuxtLink>
       <div class="space-x-2 flex items-center">
         <UButton>Docs</UButton>
-        <UButton icon="tabler:brand-github" size="md" color="primary" variant="soft" />
+        <UButton icon="tabler:brand-github" size="md" color="primary" variant="soft"/>
       </div>
     </div>
   </div>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -13,17 +13,37 @@
     <div class="my-section-wrapper mt-12">
       <h4>- Sizni kutyapti</h4>
       <div class="grid grid-cols-2 gap-5 mt-4">
-        <NuxtLink href="/lessons/golang" class="bg-primary-500/10 p-4 rounded-xl text-center">
-          <UIcon name="tabler:brand-golang" class="text-5xl"/>
+        <NuxtLink href="/lessons/golang" class="bg-primary-500/10 p-4 rounded-xl flex items-center justify-center gap-2">
+          <UIcon name="tabler:brand-golang" class="text-3xl"/>
+          <h4 class="font-bold">Go</h4>
         </NuxtLink>
-        <div class="bg-primary-500/10 p-4 rounded-xl text-center">
-          <UIcon name="tabler:brand-nuxt" class="text-5xl"/>
+        <div class="bg-primary-500/10 p-4 rounded-xl flex items-center justify-center gap-2">
+          <UIcon name="tabler:brand-nuxt" class="text-3xl"/>
+          <h4 class="font-bold">Nuxt</h4>
         </div>
-        <div class="bg-primary-500/10 p-4 rounded-xl text-center">
-          <UIcon name="tabler:brand-vue" class="text-5xl"/>
+        <div class="bg-primary-500/10 p-4 rounded-xl flex items-center justify-center gap-2">
+          <UIcon name="tabler:brand-vue" class="text-3xl"/>
+          <h4 class="font-bold">Vue</h4>
         </div>
-        <div class="bg-primary-500/10 p-4 rounded-xl text-center">
-          <UIcon name="tabler:brand-golang" class="text-5xl"/>
+        <div class="bg-primary-500/10 p-4 rounded-xl flex items-center justify-center gap-2">
+          <UIcon name="tabler:brand-javascript" class="text-3xl"/>
+          <h4 class="font-bold">JavaScript</h4>
+        </div>
+        <div class="bg-primary-500/10 p-4 rounded-xl flex items-center justify-center gap-2">
+          <UIcon name="tabler:brand-typescript" class="text-3xl"/>
+          <h4 class="font-bold">TypeScript</h4>
+        </div>
+        <div class="bg-primary-500/10 p-4 rounded-xl flex items-center justify-center gap-2">
+          <UIcon name="tabler:snowflake" class="text-3xl"/>
+          <h4 class="font-bold">nix</h4>
+        </div>
+        <div class="bg-primary-500/10 p-4 rounded-xl flex items-center justify-center gap-2">
+          <UIcon name="tabler:brand-react" class="text-3xl"/>
+          <h4 class="font-bold">React</h4>
+        </div>
+        <div class="bg-primary-500/10 p-4 rounded-xl flex items-center justify-center gap-2">
+          <UIcon name="tabler:brand-nextjs" class="text-3xl"/>
+          <h4 class="font-bold">NextJS</h4>
         </div>
       </div>
     </div>
