d5831f0d111b9c1ed889d3fd886d6af4a21d043b
Javoxir G'ulomjonov <javohirtech@gmail.com>
2025-10-02 22:27:01 +0500

    feat: add new lessons to the lessons constant for enhanced learning resources

--- app/constants/lessons.ts
+++ app/constants/lessons.ts
@@ -1,10 +1,11 @@
 export const lessons = [
   { title: "Go", icon: "tabler:brand-golang", link: "/examples/golang" },
   { title: "NuxtJS", icon: "tabler:brand-nuxt", link: "/examples/nuxtjs" },
+  { title: "nix", icon: "tabler:snowflake", link: "/examples/nix" },
+  { title: "NestJS", icon: "tabler:brand-nest", link: "/examples/nestjs" },
+  { title: "TypeScript", icon: "tabler:brand-typescript", link: "/examples/typescript" },
   { title: "Vue", icon: "tabler:brand-vue", link: "/examples/vuejs" },
   { title: "JavaScript", icon: "tabler:brand-javascript", link: "/examples/javascript" },
-  { title: "TypeScript", icon: "tabler:brand-typescript", link: "/examples/typescript" },
-  { title: "nix", icon: "tabler:snowflake", link: "/examples/nix" },
   { title: "React", icon: "tabler:brand-react", link: "/examples/react" },
   { title: "NextJS", icon: "tabler:brand-nextjs", link: "/examples/nextjs" },
   { title: "PHP", icon: "tabler:brand-php", link: "/examples/php" },
