6130dcc630483e8efece5ac18ea143b4c7a11b70
Javoxir G'ulomjonov <javohirtech@gmail.com>
2025-10-02 10:13:31 +0500

    feat: update app footer to include a "Buy me a coffee" button and improve layout for better user engagement

--- app/components/app-footer/AppFooter.vue
+++ app/components/app-footer/AppFooter.vue
@@ -2,13 +2,19 @@
   <div class="border-t border-t-primary-500/10">
     <div class="my-container flex items-center justify-between">
       <span>
-        {{date.getFullYear()}} · by <a href="https://nolan.uz" class="underline" target="_blank">nnolan</a>
+        {{ date.getFullYear() }} · by
+        <a href="https://nolan.uz" class="underline" target="_blank">nnolan</a>
       </span>
 
-      <ToggleMode size="sm"/>
+      <div class="flex items-center gap-2">
+        <UButton target="_blank" color="warning" size="sm" href="https://buymeacoffee.com/nnolan"
+          >Buy me a coffee</UButton
+        >
+        <ToggleMode size="sm" />
+      </div>
     </div>
   </div>
 </template>
 <script setup lang="ts">
-  const date = new Date();
+const date = new Date();
 </script>
