9e05b4fc0a6eeed86cdc6143425ebc2ea4fbb62b
3nln <javohirtech@gmail.com>
2026-01-02 15:39:03 +0500

    chore: replace hardcoded npmDepsHash with pkgs.lib.fakeHash for improved maintainability

--- default.nix
+++ default.nix
@@ -23,7 +23,7 @@ in
     version = manifest.version or "1.0.0";
 
     src = ./.;
-    npmDepsHash = "sha256-YuQ9UJZWV93emi5Q+M6DTN0wlyWaYUTETvPt85quGMs=";
+    npmDepsHash = pkgs.lib.fakeHash;
 
     # Rebuild native dependencies in build phase where we have write access
     preBuild = ''
